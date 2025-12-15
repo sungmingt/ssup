71cb268db54eee0d0488fe1c449e0a33ba85f9a2
sungmingt <qotjdals8313@naver.com>
2025-12-15 11:35:33 +0900

    refactor: separate core fields

--- backend/src/main/java/com/ssup/backend/global/config/S3Config.java
+++ backend/src/main/java/com/ssup/backend/global/config/S3Config.java
@@ -1,10 +1,8 @@
 package com.ssup.backend.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -14,12 +12,15 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
+    @Value("${cloud.aws.region.static}")
+    private String region;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
     @Bean
-    public S3Client s3Client(
-            @Value("${cloud.aws.region.static}") String region,
-            @Value("${cloud.aws.credentials.access-key}") String accessKey,
-            @Value("${cloud.aws.credentials.secret-key}") String secretKey
-    ) {
+    public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(
