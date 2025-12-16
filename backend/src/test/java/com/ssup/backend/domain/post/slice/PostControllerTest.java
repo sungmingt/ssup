96d330abde69da4ca26531cf99a2117303a7aa91
sungmingt <qotjdals8313@naver.com>
2025-12-16 15:00:06 +0900

    fix: downgrade spring boot version

--- backend/src/test/java/com/ssup/backend/domain/post/slice/PostControllerTest.java
+++ backend/src/test/java/com/ssup/backend/domain/post/slice/PostControllerTest.java
@@ -13,10 +13,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -34,7 +34,7 @@ class PostControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     PostService postService;
 
     @Autowired
