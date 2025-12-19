cf6d46328a1776febc7537104832adb3e451f65d
sungmingt <qotjdals8313@naver.com>
2025-12-20 00:44:37 +0900

    feat: add front origin on allowedOrigins

--- backend/src/main/java/com/ssup/backend/global/config/WebConfig.java
+++ backend/src/main/java/com/ssup/backend/global/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("*")
+                .allowedOrigins("http://localhost:5173", "http://ssup.site")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
