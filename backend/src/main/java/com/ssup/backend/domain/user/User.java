9890b4d4ec202dbfae0ce776de99396d9deb4cda
sungmingt <qotjdals8313@naver.com>
2025-12-23 15:21:03 +0900

    feat: add user domain

--- backend/src/main/java/com/ssup/backend/domain/user/User.java
+++ backend/src/main/java/com/ssup/backend/domain/user/User.java
@@ -1,5 +1,10 @@
 package com.ssup.backend.domain.user;
 
+import com.ssup.backend.domain.comment.Comment;
+import com.ssup.backend.domain.heart.comment.CommentHeart;
+import com.ssup.backend.domain.heart.post.PostHeart;
+import com.ssup.backend.domain.language.UserLanguage;
+import com.ssup.backend.domain.location.Location;
 import com.ssup.backend.domain.post.Post;
 import com.ssup.backend.global.BaseTimeEntity;
 import jakarta.persistence.*;
@@ -24,53 +29,54 @@ public class User extends BaseTimeEntity {
     @Builder.Default
     private List<Post> posts = new ArrayList<>();
 
-    //    @OneToMany(mappedBy = "user")
-//    private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "author")
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
 
-//    @OneToMany(mappedBy = "user")
-//    private List<Heart> hearts = new ArrayList<>();
+    @OneToMany(mappedBy = "user")
+    @Builder.Default
+    private List<PostHeart> postHearts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    @Builder.Default
+    private List<CommentHeart> commentHearts = new ArrayList<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @Column(length = 10, nullable = false)
-    private String nickname;
-
-//    @Column(nullable = false, unique = true)
-    private String email;
-
-    private String password;
-
-//    @Enumerated(EnumType.STRING)
-//    private SocialType socialType;
-
-    private String socialId;
-
-    private Integer age;
-
-//    @Enumerated(EnumType.STRING)
-//    private Gender gender;
-
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "location_id")
-//    private Location location;
-
     @Column(length = 30)
     private String imageUrl;
 
+    @Column(length = 10, nullable = false)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    private String socialId;
+
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
     private String intro;
 
-    @Column(length = 50)
+    @Column(length = 70)
     private String contact;
 
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//    @Builder.Default
-//    private List<UserLanguage> languages = new ArrayList<>();
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<UserLanguage> languages = new ArrayList<>();
 }
