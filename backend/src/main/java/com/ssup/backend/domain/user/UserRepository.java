07a2d2f489c86ffe035e6cbf8e447323b3d47d65
sungmingt <qotjdals8313@naver.com>
2025-12-23 23:24:02 +0900

    feat: add user find queries

--- backend/src/main/java/com/ssup/backend/domain/user/UserRepository.java
+++ backend/src/main/java/com/ssup/backend/domain/user/UserRepository.java
@@ -1,6 +1,22 @@
 package com.ssup.backend.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByEmail(String email);
+    Boolean existsByNickname(String nickname);
+
+    //N+1 제거
+    @Query("""
+    select u from User u
+    join fetch u.languages ul
+    join fetch ul.language
+    where u.id = :userId
+    """)
+    Optional<User> findWithLanguages(@Param("userId") Long userId);
 }
