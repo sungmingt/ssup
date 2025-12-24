afbc355791f471f21e2d9c8a617fa53d83d4bfb7
sungmingt <qotjdals8313@naver.com>
2025-12-24 22:19:17 +0900

    feat: add user interest domain

---
+++ backend/src/main/java/com/ssup/backend/domain/interest/InterestRepository.java
@@ -0,0 +1,6 @@
+package com.ssup.backend.domain.interest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+}
