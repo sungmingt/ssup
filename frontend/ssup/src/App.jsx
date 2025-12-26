03c2d17f8de6acbaab881134f240a9d223a9404d
sungmingt <qotjdals8313@naver.com>
2025-12-26 19:29:36 +0900

    feat: add user related components

--- frontend/ssup/src/App.jsx
+++ frontend/ssup/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LoginForm from "./components/LoginForm";
+import LoginForm from "./components/LoginForm";
 import PostList from "./components/postList/PostList";
-// import Login from "./components/Login";
-// import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Profile from "./components/user/Profile";
 import PostCreateForm from "./components/post/PostCreateForm";
 import PostUpdateForm from "./components/post/PostUpdateForm";
 import Post from "./components/post/Post";
 import Header from "./components/Header";
-// import SignUp from "./components/auth/SignUp";
+import SignUp from "./components/auth/SignUp";
+import SignUpAdditional from "./components/auth/SignUpAdditional";
 import MainPage from "./components/MainPage.jsx";
 import "./App.css";
 
@@ -24,6 +25,14 @@ function App() {
           <Route path="/posts" element={<PostList />} />
           <Route path="/posts/create" element={<PostCreateForm />} />
           <Route path="/posts/:id/update" element={<PostUpdateForm />} />
+
+          <Route path="/profile" element={<Profile isMyProfile />} />
+          <Route path="/users/:id/profile" element={<Profile />} />
+
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup/additional" element={<SignUpAdditional />} />
+
+          <Route path="/login" element={<Login />} />
         </Routes>
         {/* <Routes>
           <Route path="/" element={<MainPage />} />
