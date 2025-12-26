1ec0a621079bc29063a251a3c6a168a847567240
sungmingt <qotjdals8313@naver.com>
2025-12-26 19:30:08 +0900

    refactor: prevent N+1

--- backend/src/main/java/com/ssup/backend/domain/interest/InterestRepository.java
+++ backend/src/main/java/com/ssup/backend/domain/interest/InterestRepository.java
@@ -2,5 +2,10 @@ package com.ssup.backend.domain.interest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterestRepository extends JpaRepository<Interest, Long> {
+
+    //N+1 방지
+    List<Interest> findAllById(Iterable<Long> ids);
 }
