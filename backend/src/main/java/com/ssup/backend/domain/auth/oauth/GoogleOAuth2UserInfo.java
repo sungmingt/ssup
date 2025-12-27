6f8c4ebfc5065117f61bf89c6595bde82fc761b7
sungmingt <qotjdals8313@naver.com>
2025-12-27 22:35:41 +0900

    feat: add custom OAuth2UserInfo, service

---
+++ backend/src/main/java/com/ssup/backend/domain/auth/oauth/GoogleOAuth2UserInfo.java
@@ -0,0 +1,35 @@
+package com.ssup.backend.domain.auth.oauth;
+
+import com.ssup.backend.domain.user.SocialType;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo implements OAuth2UserInfo{
+
+    private final Map<String, Object> attributes;
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public SocialType getSocialType() {
+        return SocialType.GOOGLE;
+    }
+
+    @Override
+    public String getSocialId() {
+        return (String) attributes.get("sub");
+    }
+
+}
---
+++ backend/src/main/java/com/ssup/backend/domain/auth/oauth/OAuth2UserInfoFactory.java
@@ -0,0 +1,19 @@
+package com.ssup.backend.domain.auth.oauth;
+
+import com.ssup.backend.global.exception.ErrorCode;
+import com.ssup.backend.global.exception.SsupException;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo create(String registrationId,
+                                        Map<String, Object> attributes) {
+
+        return switch (registrationId) {
+            case "google" -> new GoogleOAuth2UserInfo(attributes);
+            case "kakao" -> new KakaoOAuth2UserInfo(attributes);
+            default -> throw new SsupException(ErrorCode.ILLEGAL_REGISTRATION_ID);
+        };
+    }
+}
