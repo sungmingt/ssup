b5df5b4d8fc3e4e820e99bfcb3b3e402648d76e2
sungmingt <qotjdals8313@naver.com>
2025-12-27 14:04:37 +0900

    feat: add user/language api

--- frontend/ssup/src/api/language.api.js
+++ frontend/ssup/src/api/language.api.js
@@ -1,6 +1,7 @@
 import { api } from "./axios";
 
 export const languageApi = {
+  getLanguageList: (userId) => api.get(`/languages`),
   getUserLanguages: (userId) => api.get(`/users/${userId}/languages`),
-  updateUserLanguages: () => api.put("/users/me/languages"),
+  updateMyLanguages: (formData) => api.put("/users/me/languages", formData),
 };
--- frontend/ssup/src/api/profile.api.js
+++ frontend/ssup/src/api/profile.api.js
@@ -4,4 +4,6 @@ export const profileApi = {
   getMyProfile: () => api.get("/users/me/profile"),
   getUserProfile: (userId) => api.get(`/users/${userId}/profile`),
   createMyProfile: (formData) => api.post("/users/me/profile", formData),
+  updateMyProfile: (formData) => api.put("/users/me/profile", formData),
+  deleteMyProfile: () => api.delete("/users/me/profile"),
 };
