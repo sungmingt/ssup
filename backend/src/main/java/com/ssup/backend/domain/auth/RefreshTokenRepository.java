bc4a2d547b359a78e0052334fe284f486c22585d
sungmingt <qotjdals8313@naver.com>
2025-12-29 00:21:00 +0900

    refactor: hashOps -> valueOps

--- backend/src/main/java/com/ssup/backend/domain/auth/RefreshTokenRepository.java
+++ backend/src/main/java/com/ssup/backend/domain/auth/RefreshTokenRepository.java
@@ -1,38 +1,43 @@
 package com.ssup.backend.domain.auth;
 
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
+import java.time.Duration;
 import java.util.Optional;
 
-import static com.ssup.backend.infra.security.jwt.TokenInfo.REFRESH_TOKEN;
 import static com.ssup.backend.infra.security.jwt.TokenInfo.REFRESH_TOKEN_VALIDATION_SECOND;
-import static java.time.Duration.ofMillis;
 
 @Repository
 public class RefreshTokenRepository {
 
-    private static final String KEY = REFRESH_TOKEN;
+    private static final String KEY_PREFIX = "refreshToken:";
     private final RedisTemplate<String, String> refreshTokenRedisTemplate;
-    private final HashOperations<String, String, String> hashOperations;
+    private final ValueOperations<String, String> ops;
 
     public RefreshTokenRepository(RedisTemplate<String, String> refreshTokenRedisTemplate) {
         this.refreshTokenRedisTemplate = refreshTokenRedisTemplate;
-        this.hashOperations = refreshTokenRedisTemplate.opsForHash();
+        ops = refreshTokenRedisTemplate.opsForValue();
+    }
+
+    private String getKey(Long userId) {
+        return KEY_PREFIX + userId;
     }
 
     public Optional<String> findByUserId(Long userId) {
-        return Optional.ofNullable(hashOperations.get(KEY, String.valueOf(userId)));
+        return Optional.ofNullable(ops.get(getKey(userId)));
     }
 
-    public String save(Long id, String refreshToken) {
-        hashOperations.put(KEY, String.valueOf(id), refreshToken);
-        refreshTokenRedisTemplate.expire(KEY, ofMillis(REFRESH_TOKEN_VALIDATION_SECOND));
-        return refreshToken;
+    public void save(Long userId, String refreshToken) {
+        ops.set(
+                getKey(userId),
+                refreshToken,
+                Duration.ofMillis(REFRESH_TOKEN_VALIDATION_SECOND)
+        );
     }
 
-    public void deleteById(Long id) {
-        hashOperations.delete(KEY, String.valueOf(id));
+    public void deleteById(Long userId) {
+        refreshTokenRedisTemplate.delete(getKey(userId));
     }
 }
