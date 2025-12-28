edcf6cdea66117119263edd861b27a976ee208fb
sungmingt <qotjdals8313@naver.com>
2025-12-28 15:24:57 +0900

    refactor: change language api

--- frontend/ssup/src/api/language.api.js
+++ frontend/ssup/src/api/language.api.js
@@ -1,7 +1,7 @@
 import { api } from "./axios";
 
 export const languageApi = {
-  getLanguageList: (userId) => api.get(`/languages`),
+  getLanguageList: () => api.get(`/languages`),
   getUserLanguages: (userId) => api.get(`/users/${userId}/languages`),
   updateMyLanguages: (formData) => api.put("/users/me/languages", formData),
 };
