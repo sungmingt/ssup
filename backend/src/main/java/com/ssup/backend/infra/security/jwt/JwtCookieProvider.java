a489637c4814f5c12d13421377541892673118bb
sungmingt <qotjdals8313@naver.com>
2025-12-28 21:20:42 +0900

    feat: add cookie handling logic

--- backend/src/main/java/com/ssup/backend/infra/security/jwt/JwtCookieProvider.java
+++ backend/src/main/java/com/ssup/backend/infra/security/jwt/JwtCookieProvider.java
@@ -1,14 +1,21 @@
 package com.ssup.backend.infra.security.jwt;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Optional;
 
 import static com.ssup.backend.infra.security.jwt.TokenInfo.*;
 
 @Component
 public class JwtCookieProvider {
+
+    public static String COOKIE_HEADER = "Set-Cookie";
 
     public ResponseCookie createAccessTokenCookie(String token) {
         return ResponseCookie.from(ACCESS_TOKEN, token)
@@ -26,17 +33,40 @@ public class JwtCookieProvider {
                 .secure(true)
                 .sameSite("None")
                 .path("/")
-                .maxAge(Duration.ofDays(14))
+                .maxAge(Duration.ofDays(1))
                 .build();
     }
 
-    public ResponseCookie deleteCookie(String name) {
-        return ResponseCookie.from(name, "")
+    public ResponseCookie reissueAccessTokenCookie(String token) {
+        return ResponseCookie.from(ACCESS_TOKEN, token)
                 .httpOnly(true)
                 .secure(true)
                 .sameSite("None")
                 .path("/")
+                .maxAge(Duration.ofMinutes(30))
+                .build();
+    }
+
+    public Optional<String> getTokenFromCookie(HttpServletRequest request, String name) {
+        if (request.getCookies() == null) return Optional.empty();
+        return Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals(name))
+                .map(Cookie::getValue)
+                .findFirst();
+    }
+
+    public void deleteAuthCookies(HttpServletResponse response) {
+        ResponseCookie access = ResponseCookie.from(ACCESS_TOKEN, "")
+                .path("/")
                 .maxAge(0)
                 .build();
+
+        ResponseCookie refresh = ResponseCookie.from(REFRESH_TOKEN, "")
+                .path("/")
+                .maxAge(0)
+                .build();
+
+        response.addHeader("Set-Cookie", access.toString());
+        response.addHeader("Set-Cookie", refresh.toString());
     }
 }
