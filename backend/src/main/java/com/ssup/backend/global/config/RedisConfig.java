4447161e71b472401dedd736e58856d04cbcb97f
sungmingt <qotjdals8313@naver.com>
2025-12-29 00:21:16 +0900

    feat: add refreshToken redisTemplate

---
+++ backend/src/main/java/com/ssup/backend/global/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.ssup.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Primary
+    @Bean
+    public RedisTemplate<String, String> refreshTokenRedisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(cf);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
