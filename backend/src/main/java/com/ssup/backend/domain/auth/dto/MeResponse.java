88c1acf75591740dfff87e3b48ebc4e5a479c325
sungmingt <qotjdals8313@naver.com>
2025-12-29 14:06:59 +0900

    refactor: change meResponse dto

--- backend/src/main/java/com/ssup/backend/domain/auth/dto/MeResponse.java
+++ backend/src/main/java/com/ssup/backend/domain/auth/dto/MeResponse.java
@@ -1,16 +1,29 @@
 package com.ssup.backend.domain.auth.dto;
 
+import com.ssup.backend.domain.user.User;
 import com.ssup.backend.domain.user.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MeResponse {
 
     private Long id;
+    private String nickname;
+    private String imageUrl;
     private UserStatus status;
 
-    public MeResponse(Long id, UserStatus status) {
-        this.id = id;
-        this.status = status;
+    public static MeResponse of(User user) {
+        return MeResponse.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .imageUrl(user.getImageUrl())
+                .status(user.getStatus())
+                .build();
     }
 }
