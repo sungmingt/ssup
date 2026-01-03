b1f7f9ce3338c7b43990ab5cf6bdb540c725672b
sungmingt <qotjdals8313@naver.com>
2026-01-03 12:26:15 +0900

    feat: add match api

--- frontend/ssup/src/api/match.api.js
+++ frontend/ssup/src/api/match.api.js
@@ -1,5 +1,7 @@
 import { api } from "./axios";
 
 export const matchApi = {
-  sendRequest: (formData) => api.get("match/request", formData),
+  sendRequest: (formData) => api.post("/matches", formData),
+  acceptRequest: (id) => api.post(`/matches/${id}/accept`),
+  rejectRequest: (id) => api.post(`/matches/${id}/reject`),
 };
