d04a037bc6254cbab965bf7ef2dba9bb90e1cb37
sungmingt <qotjdals8313@naver.com>
2026-01-04 12:20:12 +0900

    feat: add match find list api

--- frontend/ssup/src/api/match.api.js
+++ frontend/ssup/src/api/match.api.js
@@ -4,4 +4,5 @@ export const matchApi = {
   sendRequest: (formData) => api.post("/matches", formData),
   acceptRequest: (id) => api.post(`/matches/${id}/accept`),
   rejectRequest: (id) => api.post(`/matches/${id}/reject`),
+  getMyMatches: () => api.get("/users/me/matches"),
 };
