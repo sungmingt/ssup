bcd893f9440b1c70d730179b87870a16dd8debf6
sungmingt <qotjdals8313@naver.com>
2026-01-08 23:06:22 +0900

    refactor: change heartResponse access level

--- backend/src/main/java/com/ssup/backend/domain/heart/dto/HeartResponse.java
+++ backend/src/main/java/com/ssup/backend/domain/heart/dto/HeartResponse.java
@@ -1,16 +1,17 @@
 package com.ssup.backend.domain.heart.dto;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
 public class HeartResponse {
 
-    private final boolean hearted;   //현재 내가 누른 상태인지
-    private final long heartCount;   //최신 좋아요 수
+    private boolean hearted;   //현재 내가 누른 상태인지
+    private long heartCount;   //최신 좋아요 수
 
     public static HeartResponse of(boolean hearted, long heartCount) {
         return new HeartResponse(hearted, heartCount);
