979f00b4796b1e0a0f0172b86c8d5aa5db5e300c
sungmingt <qotjdals8313@naver.com>
2026-01-08 23:04:55 +0900

    refactor: remove unused error code

--- backend/src/main/java/com/ssup/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/ssup/backend/global/exception/ErrorCode.java
@@ -7,19 +7,15 @@ public enum ErrorCode {
 
     //USER
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    USER_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "로그인되지 않은 사용자입니다."),
     LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "로그인이 필요한 기능입니다."),
     USER_STATUS_PENDING(HttpStatus.FORBIDDEN, "추가 정보를 입력해주세요."),
     DELETED_USER(HttpStatus.FORBIDDEN, "탈퇴한 사용자 계정입니다."),
-    USER_ALREADY_AUTHORIZED(HttpStatus.BAD_REQUEST, "이미 로그인 상태입니다."),
     NOT_RESOURCE_OWNER(HttpStatus.FORBIDDEN, "해당 리소스의 소유자가 아닙니다."),
 
     //USER INFO
     NICKNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용중인 닉네임입니다."),
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
     EMAIL_NOT_EXISTS(HttpStatus.BAD_REQUEST, "존재하지 않는 이메일입니다."),
-    EMAIL_NOT_VALID(HttpStatus.BAD_REQUEST, "이메일 형식이 올바르지 않습니다."),
-    PASSWORD_NOT_VALID(HttpStatus.BAD_REQUEST, "비밀번호 형식이 올바르지 않습니다."),
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
 
     //AUTH
@@ -27,7 +23,6 @@ public enum ErrorCode {
     INVALID_SOCIAL_TYPE(HttpStatus.BAD_REQUEST, "올바르지 않은 소셜 로그인 제공자입니다."),
 
     //TOKEN
-    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 access token입니다."),
     REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 refresh token입니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
 
@@ -35,16 +30,12 @@ public enum ErrorCode {
     REFRESH_TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 일치하지 않습니다."),
 
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     TOKEN_SIGNATURE_INVALID(HttpStatus.UNAUTHORIZED, "올바르지 않은 토큰 시그니처입니다."),
 
-    TOKEN_FROM_BLACKLIST(HttpStatus.UNAUTHORIZED, "blacklist에 등록된 토큰입니다."),
     TOKEN_REISSUED(HttpStatus.OK, "토큰이 재발급되었습니다."),
 
     //FILE
-    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지가 존재하지 않습니다."),
-    FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일이 존재하지 않습니다."),
     FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "5MB 이하의 이미지만 업로드할 수 있습니다."),
 
     //MATCH
