468b0edcfce0a900f033957ff457b51e491a37f0
sungmingt <qotjdals8313@naver.com>
2026-04-11 13:21:08 +0900

    feat: add user profile embedding service

---
+++ backend/src/main/java/com/ssup/backend/domain/user/recommend/UserProfileEmbeddingService.java
@@ -0,0 +1,52 @@
+package com.ssup.backend.domain.user.recommend;
+
+import com.ssup.backend.domain.user.User;
+import com.ssup.backend.domain.user.UserRepository;
+import com.ssup.backend.domain.user.recommend.util.UserProfileTextBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserProfileEmbeddingService {
+
+    private final OpenAiClient openAiClient;
+    private final UserRepository userRepository;
+    private final UserProfileEmbeddingRepository repository;
+    private final UserProfileTextBuilder profileTextBuilder;
+
+    //embedding API 호출/생성/저장
+    public void rebuild(Long userId) {
+        User user = userRepository.findUserProfileById(userId)
+                .orElseThrow();
+
+        String profileText = profileTextBuilder.build(user);
+        log.info("### profileText:{}", profileText);
+
+        log.info("### OpenAI embedding 요청 호출");
+//        List<Double> embedding = openAiClient.embed(profileText);
+
+        List<Double> embedding = List.of(
+                (double) profileText.length() % 10,
+                (double) profileText.hashCode() % 100 / 100.0,
+                Math.random()
+        );
+
+        log.info("### created embedding:{}", embedding);
+        repository.save(
+                new UserProfileEmbedding(
+                        userId,
+                        embedding,
+                        LocalDateTime.now()
+                )
+        );
+
+        UserProfileEmbedding userProfileEmbedding = repository.findByUserId(userId).get();
+        log.info("### saved userProfileEmbedding:{}", userProfileEmbedding);
+    }
+}
